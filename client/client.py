74a62bb7af0eff3debddd8d576fff15ff91d2f28
Ian Havenaar <ihavenaa@nd.edu>
2022-03-13 17:03:16 -0400

    new client

--- client/client.py
+++ client/client.py
@@ -1,17 +1,24 @@
-import socket
+import socket                   # Import socket module
 
-#Send file
-with open(text_file, 'rb') as fs: 
-    #Using with, no file close is necessary, 
-    #with automatically handles file close
-    csFT.send(b'BEGIN')
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+port = 60000                    # Reserve a port for your service.
+
+s.connect((host, port))
+s.send("Hello server!")
+
+with open('received_file', 'wb') as f:
+    print 'file opened'
     while True:
-        data = fs.read(1024)
-        print('Sending data', data.decode('utf-8'))
-        csFT.send(data)
-        print('Sent data', data.decode('utf-8'))
+        print('receiving data...')
+        data = s.recv(1024)
+        print('data=%s', (data))
         if not data:
-            print('Breaking from sending data')
             break
-    csFT.send(b'ENDED') # I used the same size of the BEGIN token
-    fs.close()
+        # write data to a file
+        f.write(data)
+
+f.close()
+print('Successfully get the file')
+s.close()
+print('connection closed')
